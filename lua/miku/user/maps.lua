03c75d306b8ea5b74790b1a6faf5612ebe42af8a
MIKU-7437 <nurshanov.dias@gmail.com>
2024-11-21 17:10:43 +0500

    fix: natural error scroll

--- lua/miku/user/maps.lua
+++ lua/miku/user/maps.lua
@@ -1,19 +1,19 @@
 -- Navigation and cursor movement
-map('n', 'J', [[m'Jz]])      -- Join lines without moving cursor
-map('n', '<C-d>', '<C-d>zz') -- Scroll down and center cursor
-map('n', '<C-u>', '<C-u>zz') -- Scroll up and center cursor
-map('n', 'n', 'nzzzv')       -- Move to next search result and center cursor
-map('n', 'N', 'Nzzzv')       -- Move to prev search result and center cursor
+map("n", "J", [[m'Jz]])      -- Join lines without moving cursor
+map("n", "<C-d>", "<C-d>zz") -- Scroll down and center cursor
+map("n", "<C-u>", "<C-u>zz") -- Scroll up and center cursor
+map("n", "n", "nzzzv")       -- Move to next search result and center cursor
+map("n", "N", "Nzzzv")       -- Move to prev search result and center cursor
 
 -- Clipboard and deletion behavior
-map('x', '<leader>p', [['_dP]])         -- Paste over selections without yanking it
+map("x", "<leader>p", [['_dP]])         -- Paste over selections without yanking it
 map({ "n", "v" }, "<leader>y", [["+y]]) -- Yank to system clipboard
 map("n", "<leader>Y", [["+Y]])          -- Yank entire line to system clipboard
 map({ "n", "v" }, "<leader>d", [["_d]]) -- Delete without yanking it
 
 -- LSP diagnostics nagigation
-map("n", "<leader>k", vim.diagnostic.goto_next) -- Go to next diagnostic
-map("n", "<leader>j", vim.diagnostic.goto_prev) -- Go to prev diagnostic
+map("n", "<leader>k", vim.diagnostic.goto_prev) -- Go to next diagnostic
+map("n", "<leader>j", vim.diagnostic.goto_next) -- Go to prev diagnostic
 
 -- Move block of text in visual mode
 map("v", "J", ":m '>+1<CR>gv=gv") -- Move selected text on 1 line down
@@ -26,7 +26,7 @@ map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) --
 map("n", "<leader>x", "<cmd>!chmod +x %<CR>") -- Make file executable
 
 -- Reload and sace configuration
-map('n', '<leader><leader>', ':source $MYVIMRC<CR>') -- Reload current config
+map("n", "<leader><leader>", ":source $MYVIMRC<CR>") -- Reload current config
 map("n", "<leader>w", "<CMD>update<CR>")             -- Save current file
 
 -- Quit command
